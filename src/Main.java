a3ebf3ac840538f4fafd5cd67356cf1a42b8abf3
Marekm <marekmichal1991@gmail.com>
2021-09-25 12:51:39 +0200

    Cw 4

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,27 @@ public class Main {
     public static void main(String[] args) {
 
         /* cw.4  */
+        System.out.println("Ćw 4");
 
         /* zad.1 napisać 3 pętle for i użyc:
         a) w pierwszej pętli break
         b) w drugiej pętli continue
         c) w trzeciej pętli return
         */
-
+        for (int i = 0; i < 100; i++) {
+            if (i == 15) {
+                break;
+            }
+        }
+        for (int i = 0; i < 100; i++) {
+            if (i == 30) {
+                continue;
+            }
+        }
+        for (int i = 0; i < 100; i++) {
+            if (i == 25) {
+                return;
+            }
+        }
     }
 }
