b7e02bcf8fd5f7f2f064f9470ebf7834005d98bc
Marekm <marekmichal1991@gmail.com>
2021-09-25 13:55:36 +0200

    Cw 6

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main(String[] args) {
 
         /* ćw_6 Listy */
-
+        System.out.println("Ćw 6");
+        Scanner scanner = new Scanner(System.in);
         /* zad.1 a) Napisać program który stworzy listę ArrayList<String> (lista tablicowa),
                 Proszę dane wprowadzać z klawiatury (można użyć pętli), lista ma mieć przynajmniej 5
                 elementów i elementami mają być nazwy zwierząt.
@@ -18,14 +19,50 @@ public class Main {
                 d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
                 elementów listy
           */
+        System.out.println("Zad 1");
+        List<String> lista1 = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Podaj nazwę zwierzęcia");
+            lista1.add(scanner.next());
+        }
+        wypiszListe(lista1);
 
+        lista1.remove(lista1.size()-1);
+        lista1.remove(lista1.size()-1);
+
+        lista1.add("Myszoskoczek");
+        lista1.add("Bażant");
+        lista1.add("Kuropatwa");
+
+        wypiszListe(lista1);
+
+        Collections.reverse(lista1);
+
+        wypiszListe(lista1);
 
          /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
                 niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
                  a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
+        System.out.println("\nZad 2");
 
+        Set<Integer> lista2 = new TreeSet<>();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Podaj liczbę");
+            lista2.add(scanner.nextInt());
+        }
+        wypiszListe2(lista2);
 
+    }
+    public static void wypiszListe(List<String> lista){
+        for(String zmienna : lista){
+            System.out.println(zmienna);
+        }
+    }
+    public static void wypiszListe2(Set<Integer> lista){
+        for(int zmienna : lista){
+            System.out.println(zmienna);
+        }
     }
 
 }
